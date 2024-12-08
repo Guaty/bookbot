95dba3a44e2cc2409a0f5b7225b588ceaaa63c0c
Guaty <bjarocha911@gmail.com>
2024-12-08 15:59:20 -0600

    add get_sorted_char_list and print report

--- main.py
+++ main.py
@@ -4,8 +4,14 @@ def main():
     # print(text)
     word_count = get_word_count(text)
     # print(f"{word_count} words were in this doc.")
-    char_counts = get_char_counts(text)
-    print(char_counts)
+    char_dict = get_char_dict(text)
+    char_list = get_sorted_char_list(char_dict)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+    for char in char_list:
+        print(f"The '{char["letter"]}' character was found {char["num"]} times")
+    print(f"-- End report --")
 
 
 def get_book_text(path):
@@ -18,7 +24,7 @@ def get_word_count(text):
     return len(words)
 
 
-def get_char_counts(text):
+def get_char_dict(text):
     lowered_string = text.lower()
     char_counts={}
     
@@ -29,6 +35,17 @@ def get_char_counts(text):
             char_counts[character] += 1
 
     return char_counts
+
+def sort_on(dict):
+    return dict["num"]
+
+def get_sorted_char_list(char_dict):
+    char_list = []
+    for char in char_dict:
+        if char.isalpha():
+            char_list.append({"letter": char, "num" : char_dict[char]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
 
 
 main()

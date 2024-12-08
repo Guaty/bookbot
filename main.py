af42fe655ce1ed880db8747ec39624caa67c037a
Guaty <bjarocha911@gmail.com>
2024-12-08 12:09:19 -0600

    add get_char_counts

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    # print(text)
     word_count = get_word_count(text)
-    print(f"{word_count} words were in this doc.")
+    # print(f"{word_count} words were in this doc.")
+    char_counts = get_char_counts(text)
+    print(char_counts)
 
 
 def get_book_text(path):
@@ -14,6 +16,19 @@ def get_book_text(path):
 def get_word_count(text):
     words = text.split()
     return len(words)
+
+
+def get_char_counts(text):
+    lowered_string = text.lower()
+    char_counts={}
+    
+    for character in lowered_string:
+        if character not in char_counts:
+            char_counts[character] = 1
+        else:
+            char_counts[character] += 1
+
+    return char_counts
 
 
 main()
